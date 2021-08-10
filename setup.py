6660cf40711a7b6c6412b3a84da7758126b691b2
iamtekson <teksondada@gmail.com>
2021-08-10 15:10:45 +0700

    v0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="postgres-helper",
-    version="0.0.2",
+    version="0.0.3",
     author="Tek Kshetri",
     author_email="iamtekson@gmail.com",
     description="Package for Postgres query on python",
